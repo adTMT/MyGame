bc85469ee408693ea152a20ff9ed184de43e5220
Adnane taamouti <Adnane.taamouti@student.ap.be>
2024-12-15 19:10:26 +0100

    muziek en startscherm

--- pong/Game1.cs
+++ pong/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using pong.Input;
 using pong.Levels;
 using System;
@@ -11,6 +12,11 @@ using static System.Formats.Asn1.AsnWriter;
 
 namespace pong
 {
+    public enum LevelSelect
+    {
+        Level1,
+        Level2
+    }
     public enum GameState
     {
         Start,
@@ -30,7 +36,7 @@ namespace pong
         Rectangle blokje2;
         Vector2 positie = new Vector2(50, 50);
         Color backgroundColor = Color.Black;
-        private GameState currentGameState = GameState.Playing;
+        private GameState currentGameState = GameState.Start;
         //hitboxtexture
         Texture2D HitboxTexture;
         //level
@@ -47,6 +53,9 @@ namespace pong
         private Texture2D coinTexture;
         private int coinsCollected = 0;
         private int coinsNeeded = 5;
+        private LevelSelect currentLevel = LevelSelect.Level1;
+        private Song backgroundMusic;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -58,6 +67,11 @@ namespace pong
         {
             // TODO: Add your initialization logic here
             base.Initialize();
+
+            //muziek
+            MediaPlayer.IsRepeating = true; // Laat de muziek herhalen
+            MediaPlayer.Volume = 0.5f;      // Stel het volume in (0.0 tot 1.0)
+            MediaPlayer.Play(backgroundMusic);
         }
 
         protected override void LoadContent()
@@ -67,9 +81,7 @@ namespace pong
             // TODO: use this.Content to load your game content here
             _texture = Content.Load<Texture2D>("Ratfolk Mage Sprite Sheet");
             InitializeGameObject();
-            //blokje
-            
-
+            backgroundMusic = Content.Load<Song>("DarkFantasy");
         }
 
         private void InitializeGameObject()
@@ -79,7 +91,14 @@ namespace pong
             blokje = new Rectangle((int)positie.X + 200, (int)positie.Y+150, 50, 100);
             blokje2 = new Rectangle((int)positie.X + 400, (int)positie.Y + 150, 50, 100);
             //
-            LoadLevel1();
+            //if (currentLevel == LevelSelect.Level1)
+            //{
+            //    LoadLevel1();
+            //}
+            //else if(currentLevel == LevelSelect.Level2)
+            //{
+            //    LoadLevel2();
+            //}
 
         }
 
@@ -87,7 +106,27 @@ namespace pong
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            if (currentGameState == GameState.Start)
+            {
+                KeyboardState state = Keyboard.GetState();
 
+                // Navigeren tussen levels
+                if (state.IsKeyDown(Keys.Left))
+                    currentLevel = LevelSelect.Level1;
+                else if (state.IsKeyDown(Keys.Right))
+                    currentLevel = LevelSelect.Level2;
+
+                // Bevestig de keuze met Enter
+                if (state.IsKeyDown(Keys.Enter))
+                {
+                    currentGameState = GameState.Playing;
+
+                    if (currentLevel == LevelSelect.Level1)
+                        LoadLevel1();
+                    else if (currentLevel == LevelSelect.Level2)
+                        LoadLevel2();
+                }
+            }
             // TODO: Add your update logic here
             if (currentGameState == GameState.Playing)
             {
@@ -137,24 +176,10 @@ namespace pong
 
         private void RestartGame()
         {
-            currentGameState = GameState.Playing;
-            firsttime = true; 
-            firsttime2 = true;
-            hero = new Hero(_texture, new KeyBoardReader()); // Reset hero
-            enemyManager = new EnemyManager(Content.Load<Texture2D>("Ghost-Sheet")); // Reset enemy manager
-            enemyManager.AddEnemy(new Vector2(200, 200), Color.White);
-            enemyManager.AddEnemy(new Vector2(50, 200), Color.White);
-            enemyManager.AddEnemy(new Vector2(50, 350), Color.White);
-            coinsCollected = 0;//resset coins
-            coinTexture = Content.Load<Texture2D>("Coin-Sheetpng");
-            coins = new List<Coin>
-                    {
-                        new Coin(new Vector2(100, 100), coinTexture),
-                        new Coin(new Vector2(750, 200), coinTexture),
-                        new Coin(new Vector2(30, 200), coinTexture),
-                        new Coin(new Vector2(50, 400), coinTexture),
-                        new Coin(new Vector2(750, 400), coinTexture)
-                    };
+            if (currentLevel == LevelSelect.Level1)
+                RestartLevel1();
+            else if (currentLevel == LevelSelect.Level2)
+                RestartLevel2();
         }
         
 
@@ -184,6 +209,10 @@ namespace pong
             else if (currentGameState == GameState.Won)
             {
                 DrawWonScreen();
+            }
+            else if (currentGameState == GameState.Start)
+            {
+                DrawStart();
             }
             _spriteBatch.End();
             base.Draw(gameTime);
@@ -245,6 +274,22 @@ namespace pong
 
             _spriteBatch.DrawString(font, coinText, position, Color.White);
         }
+        private void DrawStart()
+        {
+            var font = Content.Load<SpriteFont>("Gamefont");
+            string titleText = "Choose Your Level";
+            string level1Text = "Level 1";
+            string level2Text = "Level 2";
+            Vector2 titlePosition = new Vector2(_graphics.PreferredBackBufferWidth / 2 - font.MeasureString(titleText).X / 2,100);
+            _spriteBatch.DrawString(font, titleText, titlePosition, Color.White);
+            //
+            Vector2 level1Position = new Vector2(_graphics.PreferredBackBufferWidth / 4 - font.MeasureString(level1Text).X / 2,200);
+            _spriteBatch.DrawString(font, level1Text, level1Position, currentLevel == LevelSelect.Level1 ? Color.Yellow : Color.White);
+            //
+            Vector2 level2Position = new Vector2(3 * _graphics.PreferredBackBufferWidth / 4 - font.MeasureString(level2Text).X / 2,200);
+            _spriteBatch.DrawString(font, level2Text, level2Position, currentLevel == LevelSelect.Level2 ? Color.Yellow : Color.White);
+
+        }
         private void LoadLevel1()
         {
             blokTexture = new Texture2D(GraphicsDevice, 1, 1);
@@ -293,11 +338,96 @@ namespace pong
                                            { 1, 1, 1, 1,0,0, 1,1,1,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}};
             level.LoadLevel(levelLayout);
         }
+        private void LoadLevel2()
+        {
+            blokTexture = new Texture2D(GraphicsDevice, 1, 1);
+            blokTexture.SetData(new[] { Color.White });
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            //hitbox
+            HitboxTexture = new Texture2D(GraphicsDevice, 1, 1);
+            HitboxTexture.SetData(new[] { Color.White });
+            //enemies
+            enemyManager = new EnemyManager(Content.Load<Texture2D>("Ghost-Sheet"));
+            enemyManager.AddEnemy(new Vector2(200, 200), Color.White);
+            enemyManager.AddEnemy(new Vector2(50, 200), Color.White);
+            enemyManager.AddEnemy(new Vector2(50, 350), Color.White);
+            //enemyTexture.SetData(new[] { Color.Red });
+            //coin
+            coinTexture = Content.Load<Texture2D>("Coin-Sheetpng");
+            coins = new List<Coin>
+                    {
+                        new Coin(new Vector2(100, 100), coinTexture),
+                        new Coin(new Vector2(750, 95), coinTexture),
+                        new Coin(new Vector2(30, 200), coinTexture),
+                        new Coin(new Vector2(50, 400), coinTexture),
+                        new Coin(new Vector2(750, 400), coinTexture)
+                    };
+            //level
+            tilesetTexture = Content.Load<Texture2D>("0x72_DungeonTilesetII_v1.7");
+            Dictionary<int, Rectangle> tileMapping = new Dictionary<int, Rectangle>{
+                                                         { 0, new Rectangle(16, 64, 16, 16) },  // Vloer
+                                                         { 1, new Rectangle(16, 16, 16, 16) }}; //muur
+            level = new Level(tilesetTexture, 32, tileMapping); // Stel de grootte van de tiles in (bijvoorbeeld 32x32)
+            int[,] levelLayout = new int[,]{
+                                           { 0, 0, 1, 1,0,0, 1,1,1,1 ,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+                                           { 0, 0, 0, 0,0,0, 1,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 0, 0,0,0, 1,0,0,1 ,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+                                           { 1, 0, 0, 0,0,0, 1,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 1, 1, 1,1,0, 1,0,0,1 ,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+                                           { 1, 0, 0, 0,0,0, 0,0,0,0 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 0,0,0, 0,0,0,0 ,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
+                                           { 1, 0, 1, 0,0,0, 0,0,0,0 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 0,0,0, 0,0,0,0 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 1,0,0, 1,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 1,0,0, 1,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 0,0,0, 1,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 0,0,0, 1,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 0,0,0, 1,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                           { 1, 0, 1, 1,0,0, 1,1,1,1 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}};
+            level.LoadLevel(levelLayout);
+        }
         private void RestartLevel1()
         {
-
+            currentGameState = GameState.Playing;
+            firsttime = true;
+            firsttime2 = true;
+            hero = new Hero(_texture, new KeyBoardReader()); // Reset hero
+            enemyManager = new EnemyManager(Content.Load<Texture2D>("Ghost-Sheet")); // Reset enemy manager
+            enemyManager.AddEnemy(new Vector2(200, 200), Color.White);
+            enemyManager.AddEnemy(new Vector2(50, 200), Color.White);
+            enemyManager.AddEnemy(new Vector2(50, 350), Color.White);
+            coinsCollected = 0;//resset coins
+            coinTexture = Content.Load<Texture2D>("Coin-Sheetpng");
+            coins = new List<Coin>
+                    {
+                        new Coin(new Vector2(100, 100), coinTexture),
+                        new Coin(new Vector2(750, 200), coinTexture),
+                        new Coin(new Vector2(30, 200), coinTexture),
+                        new Coin(new Vector2(50, 400), coinTexture),
+                        new Coin(new Vector2(750, 400), coinTexture)
+                    };
         }
-        
+        private void RestartLevel2()
+        {
+            currentGameState = GameState.Playing;
+            firsttime = true;
+            firsttime2 = true;
+            hero = new Hero(_texture, new KeyBoardReader()); // Reset hero
+            enemyManager = new EnemyManager(Content.Load<Texture2D>("Ghost-Sheet")); // Reset enemy manager
+            enemyManager.AddEnemy(new Vector2(200, 200), Color.White);
+            enemyManager.AddEnemy(new Vector2(50, 200), Color.White);
+            enemyManager.AddEnemy(new Vector2(50, 350), Color.White);
+            coinsCollected = 0;//resset coins
+            coinTexture = Content.Load<Texture2D>("Coin-Sheetpng");
+            coins = new List<Coin>
+                    {
+                        new Coin(new Vector2(100, 100), coinTexture),
+                        new Coin(new Vector2(750, 95), coinTexture),
+                        new Coin(new Vector2(30, 200), coinTexture),
+                        new Coin(new Vector2(50, 400), coinTexture),
+                        new Coin(new Vector2(750, 400), coinTexture)
+                    };
+        }
 
     }
 }
