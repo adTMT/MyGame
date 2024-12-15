80fe3e4187f5949a3bb5e2aa538b1cd82ea9b098
Adnane taamouti <Adnane.taamouti@student.ap.be>
2024-12-15 17:10:11 +0100

    coin klasse

---
+++ pong/Coin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pong
+{
+    internal class Coin
+    {
+        public Vector2 Position { get; private set; }
+        public Rectangle Hitbox => new Rectangle((int)Position.X, (int)Position.Y, 16, 16);
+        private Texture2D texture;
+
+        public Coin(Vector2 position, Texture2D texture)
+        {
+            Position = position;
+            this.texture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, Color.Yellow);
+        }
+    }
+}
